6b9a05cb50aa8e9420c2de417d64fff9118da6b1
egart <ericgartner@gmail.com>
2015-12-05 16:08:19 -0800

    Single-quoted echoes to avoid a substitution, sped up ROM sync

--- mame_update.sh
+++ mame_update.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# MAME_Update v0.2
+# MAME_Update v0.3
 #
 # Script to build or update a MAME directory in OS X from source directories of ROMs, CHDs, and EXTRAs
 #
@@ -8,10 +8,11 @@
 #
 # 0.1: Initial release (12/4/2015)
 # 0.2: Preserve artwork timestamps (12/5/2015)
+# 0.3: Single-quoted echoes to avoid a substitution, sped up ROM sync
 
 #Source and destination folders
 rom_src=/Volumes/EMULATION
-mame_dest=/Volumes/Stadium Events/Games/MAME
+mame_dest=/Volumes/Stadium\ Events/Games/MAME
 
 # New version numbers
 mame_ver="0.168"
@@ -28,7 +29,7 @@ extras="$rom_src"/MAME\ $extras_ver\ EXTRAs
 cwd=$(pwd)
 
 # Download SDLMAME, catver.ini, category.ini
-echo "Downloading MAME files"
+echo 'Downloading MAME files'
 sleep 2
 mkdir -p $TMPDIR/downloads && cd $_
 curl -#O http://sdlmame.lngn.net/mame0${mame_ver: -3}-64bit.zip                                              #SDLMAME
@@ -36,7 +37,7 @@ curl -#O http://www.progettosnaps.net/MAME/pS_MAME_catver.zip                   
 curl -#o category.ini "http://sourceforge.net/p/qmc2/code/HEAD/tree/trunk/data/cat/category.ini?format=raw"  #category.ini
 
 # Installing base MAME
-echo "Installing base MAME"
+echo 'Installing base MAME'
 unzip -q mame0168-64bit.zip
 ls "$extras" > $TMPDIR/excludes
 echo "roms" >> $TMPDIR/excludes
@@ -48,12 +49,12 @@ echo "category.ini" >> $TMPDIR/excludes
 rsync -acviP --delete --exclude '*DS_Store*' --exclude-from=$TMPDIR/excludes $TMPDIR/downloads/mame0${mame_ver: -3}-64bit/ "$mame_dest"
 
 # Updating ROMs and CHDs
-echo "Updating ROMs and CHDs. This may take a while."
+echo 'Updating ROMs and CHDs. This may take a while.'
 sleep 2
-rsync -acviP --delete --exclude '*_ReadMe_*' "$roms" "$chds" "$swlist_roms" "$swlist_chds" "$mame_dest/roms"
+rsync -aviP --delete --exclude '*_ReadMe_*' "$roms" "$chds" "$swlist_roms" "$swlist_chds" "$mame_dest/roms"
 
 #Updating extras
-echo "Updating extras"
+echo 'Updating extras'
 sleep 2
 mv -f category.ini "$mame_dest"
 unzip -q pS_MAME_catver.zip
@@ -75,7 +76,7 @@ rsync -aviP --delete --exclude-from=$TMPDIR/excludes "$extras/" "$mame_dest"
 rsync -aviP "$TMPDIR/downloads/mame0${mame_ver: -3}-64bit/samples/" "$mame_dest/samples"
 
 #ARTWORK
-echo "Updating artwork"
+echo 'Updating artwork'
 sleep 2
 mkdir -p $TMPDIR/mameart
 cd "$extras/artwork"
@@ -88,19 +89,19 @@ rsync -acviP "$TMPDIR/downloads/mame0${mame_ver: -3}-64bit/artwork/" $TMPDIR/mam
 
 
 #CABINETS
-echo "Updating cabinets"
+echo 'Updating cabinets'
 sleep 2
 rsync -aviP --delete "$extras/cabinets/" "$extras/cabdevs/" "$mame_dest/cabinets"
 
 #CHEAT
-echo "Updating cheats"
+echo 'Updating cheats'
 sleep 2
 mkdir -p $TMPDIR/cheat
 7za x -o$TMPDIR/cheat "$extras/cheat.7z"
 rsync -acviP --delete $TMPDIR/cheat/ "$mame_dest/cheat"
 
 #ICONS
-echo "Updating icons"
+echo 'Updating icons'
 sleep 2
 cd "$extras/icons"
 zip -0 $TMPDIR/icons.zip *
@@ -118,7 +119,7 @@ case $qmc in
     hdiutil attach $TMPDIR/downloads/qmc2-macosx-intel-$qmc_ver.dmg;
     open /Volumes/QMC2-$qmc_ver/QMC2.mpkg;;
   [Nn]* )
-    exit;;
+    ;;
 esac
 
 #Cleanup
@@ -126,4 +127,4 @@ cd $cwd
 unset cwd rom_src mame_dest mame_ver extras_ver qmc_ver roms chds swlist_roms swlist_chds extras qmc
 if [ -d QMC2-0.59 ]; then hdiutil attach $TMPDIR/downloads/qmc2-macosx-intel-$qmc_ver.dmg; fi
 rm -r $TMPDIR/mameart $TMPDIR/excludes $TMPDIR/cheat $TMPDIR/icons.zip $TMPDIR/downloads
-echo "Script complete!"
+echo 'Script complete!'
